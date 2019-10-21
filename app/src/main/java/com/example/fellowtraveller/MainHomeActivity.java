c41163bcc5761795931233c4407da55fe5f9229d
ChrissSav <dai17023@uom.edu.gr>
2019-10-22 01:01:02 +0300

    Add PopUp Menu

--- app/src/main/java/com/example/fellowtraveller/MainHomeActivity.java
+++ app/src/main/java/com/example/fellowtraveller/MainHomeActivity.java
@@ -4,18 +4,26 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainHomeActivity extends AppCompatActivity {
-
+    private Dialog dia;
+    private Button btn_popup_menu,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainhome);
-
+        dia = new Dialog(this);
+        dia.setContentView(R.layout.popup);
+        dia.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         BottomNavigationView bottomNavigationView;
         bottomNavigationView = findViewById(R.id.home_bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
@@ -42,5 +50,22 @@ public class MainHomeActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+
+
+        btn_popup_menu = findViewById(R.id.home_button_popup_menu);
+        btn_popup_menu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                dia.show();
+            }
+        });
+
+        btn2 = dia.findViewById(R.id.popup_button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                dia.dismiss();
+            }
+        });
     }
 }

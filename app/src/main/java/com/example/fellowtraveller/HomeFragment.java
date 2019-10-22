0de1984c2a52c59f2e544a1fc01c9cddad79a8cf
ChrissSav <dai17023@uom.edu.gr>
2019-10-22 16:18:27 +0300

    Add bottom bar to newofferactivity

--- app/src/main/java/com/example/fellowtraveller/HomeFragment.java
+++ app/src/main/java/com/example/fellowtraveller/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.fellowtraveller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,9 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
-
+    private Button btn;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,6 +24,15 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        btn = v.findViewById(R.id.frag_home_button_search);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(),NewOfferActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }

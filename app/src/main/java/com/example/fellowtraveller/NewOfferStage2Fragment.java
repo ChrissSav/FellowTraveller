8d6479a6085101fb760cd0340a5516160bb93cdb
ChrissSav <dai17023@uom.edu.gr>
2019-10-22 12:23:16 +0300

    Inser NewOfferActivity

---
+++ app/src/main/java/com/example/fellowtraveller/NewOfferStage2Fragment.java
@@ -0,0 +1,31 @@
+package com.example.fellowtraveller;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewOfferStage2Fragment extends Fragment {
+
+
+    public NewOfferStage2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_offer_stage2, container, false);
+    }
+
+}

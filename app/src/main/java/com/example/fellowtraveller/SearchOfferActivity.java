d32c3faa396e1d165c681a8322939390271ae43f
ChrissSav <dai17023@uom.edu.gr>
2019-11-15 20:49:57 +0200

    Improve style in SearchActivity and Stage2 Fragment

--- app/src/main/java/com/example/fellowtraveller/SearchOfferActivity.java
+++ app/src/main/java/com/example/fellowtraveller/SearchOfferActivity.java
@@ -14,6 +14,7 @@ public class SearchOfferActivity extends AppCompatActivity {
     private Fragment fra;
     private SearchOfferStage1Fragment stage1 = new SearchOfferStage1Fragment() ;
     private SearchOfferStage2Fragment stage2 = new SearchOfferStage2Fragment();
+    private TextView header;
     private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,7 @@ public class SearchOfferActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_offer);
         fragmentManager = getSupportFragmentManager();
         btn_next_stage = findViewById(R.id.search_offer_button_next_fragment);
+        header = findViewById(R.id.search_offer_textView_header);
         btn_back = findViewById(R.id.search_offer_button_back);
         fra = stage1;
         fragmentManager.beginTransaction().replace(R.id.search_offer_container,fra).commit();
@@ -29,6 +31,7 @@ public class SearchOfferActivity extends AppCompatActivity {
             {
                 if(fra.toString().equals("stage1")){
                     fra = stage2;
+                    header.setText("Φίλτρα");
                     fragmentManager.beginTransaction().setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left).replace(R.id.search_offer_container,fra).commit();
                 }
             }
@@ -42,6 +45,7 @@ public class SearchOfferActivity extends AppCompatActivity {
                     SearchOfferActivity.this.onBackPressed();
                 }
                 else if(fra.toString().equals("stage2")) {
+                    header.setText("Αναζήτηση");
                     fra = stage1;
                     fragmentManager.beginTransaction().setCustomAnimations(R.anim.enter_from_left,R.anim.exit_to_right).replace(R.id.search_offer_container, fra).commit();
                 }

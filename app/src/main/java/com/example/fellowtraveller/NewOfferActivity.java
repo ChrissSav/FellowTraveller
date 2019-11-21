dc34406695b9b26b0a6b91296b6b0d2035914429
ChrissSav <dai17023@uom.edu.gr>
2019-11-22 01:07:01 +0200

    Update NewOfferActivity and Layout

--- app/src/main/java/com/example/fellowtraveller/NewOfferActivity.java
+++ app/src/main/java/com/example/fellowtraveller/NewOfferActivity.java
@@ -27,94 +27,21 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.Arrays;
 
 public class NewOfferActivity extends AppCompatActivity {
-    private Button btn_next_stage,btn_back;
-    private Fragment fra;
-    private TextView tvStage1,tvStage2,tvStage3;
-    private NewOfferStage1Fragment stage1 = new NewOfferStage1Fragment() ;
-    private NewOfferStage2Fragment stage2 = new NewOfferStage2Fragment();
-    private NewOfferStage3Fragment stage3 = new NewOfferStage3Fragment() ;
-    private FragmentManager fragmentManager;
-    private PlacesClient placesClient;
+    private Button btn_back;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_offer);
 
-        //PlaceAPI
-        /*String apikey = "AIzaSyDucU0rzkgk_uvxcYIWFfOXMQY0AwoS8vg";
-        if(!Places.isInitialized()){
-            Places.initialize(getApplicationContext(),apikey);
-        }
-
-        placesClient = Places.createClient(this);
-        final AutocompleteSupportFragment autocompleteSupportFragment =
-                (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
-
-        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID,Place.Field.LAT_LNG,Place.Field.NAME));
-        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                final LatLng latLng = place.getLatLng();
-                //ConsoleCheck     Log.i("PlaceAPI", "onPlaceSelected: " + latLng.latitude + "/n" + latLng.longitude);
-            }
-
-            @Override
-            public void onError(@NonNull Status status) {
-
-            }
-        });*/
-
-        fragmentManager = getSupportFragmentManager();
-        btn_next_stage = findViewById(R.id.new_offer_button_next_fragment);
         btn_back = findViewById(R.id.new_offer_button_back);
-        tvStage1 = findViewById(R.id.new_offer_textView_stage1);
-        tvStage2 = findViewById(R.id.new_offer_textView_stage2);
-        tvStage3 = findViewById(R.id.new_offer_textView_stage3);
-
-
-
-        fra = stage1;
-        fragmentManager.beginTransaction().replace(R.id.new_offer_container,fra).commit();
-        btn_next_stage.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
-            {
-                if(fra.toString().equals("stage1")){
-                    fra = stage2;
-                    tvStage1.setBackgroundResource(R.drawable.new_offer_btn_bottom_default);
-                    tvStage2.setBackgroundResource(R.drawable.new_offer_btn_bottom_click);
-                    fragmentManager.beginTransaction().setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left).replace(R.id.new_offer_container,fra).commit();
-               }
-                else if(fra.toString().equals("stage2")){
-                    fra = stage3;
-                    tvStage2.setBackgroundResource(R.drawable.new_offer_btn_bottom_default);
-                    tvStage3.setBackgroundResource(R.drawable.new_offer_btn_bottom_click);
-                    fragmentManager.beginTransaction().setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left).replace(R.id.new_offer_container,fra).commit();
-                }
-            }
-        });
-
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                if(fra.toString().equals("stage1")){
-                    NewOfferActivity.this.onBackPressed();
-                }
-                else if(fra.toString().equals("stage2")){
-                    fra = stage1;
-                    tvStage2.setBackgroundResource(R.drawable.new_offer_btn_bottom_default);
-                    tvStage1.setBackgroundResource(R.drawable.new_offer_btn_bottom_click);
-                    fragmentManager.beginTransaction().setCustomAnimations(R.anim.enter_from_left,R.anim.exit_to_right).replace(R.id.new_offer_container,fra).commit();
-                }
-                else if(fra.toString().equals("stage3")){
-                    fra = stage2;
-                    tvStage3.setBackgroundResource(R.drawable.new_offer_btn_bottom_default);
-                    tvStage2.setBackgroundResource(R.drawable.new_offer_btn_bottom_click);
-                    fragmentManager.beginTransaction().setCustomAnimations(R.anim.enter_from_left,R.anim.exit_to_right).replace(R.id.new_offer_container,fra).commit();
-                }
+                NewOfferActivity.this.onBackPressed();
             }
         });
-
     }
 }

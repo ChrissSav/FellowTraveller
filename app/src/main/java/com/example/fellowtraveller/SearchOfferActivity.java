b36e480c94939e373945fc0504818b776a162110
ChrissSav <dai17023@uom.edu.gr>
2019-11-22 00:38:40 +0200

    Update UserProfile Layout

--- app/src/main/java/com/example/fellowtraveller/SearchOfferActivity.java
+++ app/src/main/java/com/example/fellowtraveller/SearchOfferActivity.java
@@ -11,18 +11,16 @@ import android.widget.TextView;
 
 public class SearchOfferActivity extends AppCompatActivity {
     private Button btn_back;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_offer);
         btn_back = findViewById(R.id.search_offer_button_back);
-
         btn_back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                SearchOfferActivity.this.onBackPressed();
+                    SearchOfferActivity.this.onBackPressed();
+
             }
         });
     }

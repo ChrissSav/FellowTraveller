b55993cd3be9f35c03575b5406e51a34bdcadbc4
Spuros <spurakos98@gmail.com>
2019-12-04 14:07:22 +0200

    NavBar to all activities

--- app/src/main/java/com/example/fellowtraveller/Settings.java
+++ app/src/main/java/com/example/fellowtraveller/Settings.java
@@ -2,29 +2,88 @@ package com.example.fellowtraveller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.navigation.NavigationView;
 
-public class Settings extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class Settings extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String FILE_NAME = "fellow_login_state.txt";
+
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-        Button clickButton = (Button) findViewById(R.id.button2);
-        clickButton.setOnClickListener( new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent i = new Intent(Settings.this, MainHomeActivity.class);
-                startActivity(i);
-                finish();
-            }
-        });
 
+
+
+        Toolbar toolbar =  findViewById(R.id.home_appBar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView =  findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     
     }
 
+    @Override
+    public boolean onNavigationItemSelected( MenuItem item) {
+
+        closeDrawer();
+        switch (item.getItemId()){
+            case R.id.home:
+                Intent c = new Intent(Settings.this, MainHomeActivity.class);
+                startActivity(c);
+                finish();
+                break;
+
+            case R.id.profile:
+                break;
+
+            case R.id.wallet:
+                Intent s = new Intent(Settings.this, Wallet.class);
+                startActivity(s);
+                finish();
+                break;
+            case R.id.settings:
+                Intent i = new Intent(Settings.this, Settings.class);
+                startActivity(i);
+                finish();
+                break;
+            case R.id.logout:
+                Intent j = new Intent(Settings.this, MainActivity.class);
+                startActivity(j);
+                finish();
+                break;
+        }
+        return true;
+    }
+    private void closeDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+
+    private void openDrawer(){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            super.onBackPressed();
+    }
 }
--- app/src/main/java/com/example/fellowtraveller/Wallet.java
+++ app/src/main/java/com/example/fellowtraveller/Wallet.java
@@ -2,32 +2,89 @@ package com.example.fellowtraveller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.navigation.NavigationView;
 
-public class Wallet extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+
+public class Wallet extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String FILE_NAME = "fellow_login_state.txt";
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wallet);
 
-        Button clickButton = (Button) findViewById(R.id.button);
-        clickButton.setOnClickListener( new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent a = new Intent(Wallet.this, MainHomeActivity.class);
-                startActivity(a);
+
+        Toolbar toolbar =  findViewById(R.id.home_appBar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView =  findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        closeDrawer();
+        switch (item.getItemId()){
+            case R.id.home:
+                Intent c = new Intent(Wallet.this, MainHomeActivity.class);
+                startActivity(c);
                 finish();
-            }
-        });
+                break;
+
+            case R.id.profile:
+                break;
+
+            case R.id.wallet:
+                Intent s = new Intent(Wallet.this, Wallet.class);
+                finish();
+                break;
+            case R.id.settings:
+                Intent i = new Intent(Wallet.this, Settings.class);
+                startActivity(i);
+                finish();
+                break;
+            case R.id.logout:
+                Intent j = new Intent(Wallet.this, MainActivity.class);
+                startActivity(j);
+                finish();
+                break;
+        }
+        return true;
+    }
+    private void closeDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
 
 
+    private void openDrawer(){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
 
-
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            super.onBackPressed();
     }
 }

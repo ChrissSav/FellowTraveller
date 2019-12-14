e6a554578c809a12aeb871680cc901ff3b343691
Spuros <spurakos98@gmail.com>
2019-12-14 21:41:42 +0200

    Updated Home Fragment and fragment_home

--- app/src/main/java/com/example/fellowtraveller/HomeFragment.java
+++ app/src/main/java/com/example/fellowtraveller/HomeFragment.java
@@ -11,9 +11,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeFragment extends Fragment {
-    private Button btn1,btn2;
+    private ImageButton btn1,btn2;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,8 +26,8 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-        btn1 = v.findViewById(R.id.frag_home_button_offer);
-        btn2 = v.findViewById(R.id.frag_home_button_search);
+        btn1 = v.findViewById(R.id.imageButton);
+        btn2 = v.findViewById(R.id.imageButton2);
         btn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {

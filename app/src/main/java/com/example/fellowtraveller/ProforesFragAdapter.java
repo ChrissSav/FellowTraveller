cfd92b8257f0d6c790ca6b78c9b1089bf6ce8514
ChrissSav <dai17023@uom.edu.gr>
2019-12-16 21:35:01 +0200

    Prepare Prosfores Fargment

---
+++ app/src/main/java/com/example/fellowtraveller/ProforesFragAdapter.java
@@ -0,0 +1,91 @@
+package com.example.fellowtraveller;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ProforesFragAdapter extends RecyclerView.Adapter<ProforesFragAdapter.ExampleViewHolder> {
+    private ArrayList<Trip> mExampleList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+
+        void onItemClick(int position);
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView from;
+        public TextView to;
+        public TextView date;
+        public TextView time;
+        public TextView number_of_passengers;
+        public TextView number_of_bags;
+        public Button btn;
+
+        public ExampleViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            date = itemView.findViewById(R.id.searchItem_textView_date);
+            time = itemView.findViewById(R.id.searchItem_textView_time);
+            from = itemView.findViewById(R.id.searchItem_textView_from);
+            to = itemView.findViewById(R.id.searchItem_textView_to);
+            name = itemView.findViewById(R.id.searchItem_textView_name);
+            number_of_passengers = itemView.findViewById(R.id.searchItem_textView_seats);
+            number_of_bags = itemView.findViewById(R.id.searchItem_textView_bags);
+            btn = itemView.findViewById((R.id.searchItem_button_select));
+
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+
+
+                }
+            });
+        }
+    }
+
+    public ProforesFragAdapter(ArrayList<Trip> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_trip_item, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v,mListener);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        Trip currentItem = mExampleList.get(position);
+        holder.from.setText(currentItem.getFfrom());
+        holder.to.setText(currentItem.getTto());
+        holder.date.setText(currentItem.getDate());
+        holder.time.setText(currentItem.getTime());
+        holder.name.setText(currentItem.getCreator().getName());
+        holder.number_of_passengers.setText(currentItem.getSeatesStatus()+"");
+        holder.number_of_bags.setText(currentItem.getbagsStatus()+"");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+}
--- app/src/main/java/com/example/fellowtraveller/ProsforesFragment.java
+++ app/src/main/java/com/example/fellowtraveller/ProsforesFragment.java
@@ -1,23 +1,46 @@
 package com.example.fellowtraveller;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 
 public class ProsforesFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private SearchAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<Trip> Listoftrips ;
+    private JsonApi jsonPlaceHolderApi;
+    private Retrofit retrofit ;
+    private TextView textError;
+    private final String FILE_NAME = "fellow_login_state.txt";
 
     public ProsforesFragment() {
         // Required empty public constructor
@@ -28,6 +51,106 @@ public class ProsforesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prosfores, container, false);
+        View view = inflater.inflate(R.layout.fragment_prosfores, container, false);
+        return view;
+    }
+
+    public void buildRecyclerView(View v) {
+        mRecyclerView = v.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mAdapter = new SearchAdapter(Listoftrips);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(new SearchAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent intent = new Intent(getActivity(), TripPageActivity.class);
+                intent.putExtra("Trip",Listoftrips.get(position));
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+
+    private void getTrips() {
+
+        if(CheckInternetConnection()){
+            Call<List<Trip>> call = jsonPlaceHolderApi.getTripsTakesPart(loadUserId());
+            call.enqueue(new Callback<List<Trip>>() {
+                @Override
+                public void onResponse(Call<List<Trip>> mcall, Response<List<Trip>> response) {
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(getActivity(),"responseb "+response.message(),Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    textError.setText("");
+                    List<Trip> trips = response.body();
+                    for (int i=0; i<trips.size(); i++){
+                        Listoftrips.add(trips.get(i));
+                    }
+                }
+                @Override
+                public void onFailure(Call<List<Trip>> call, Throwable t) {
+                    //Toast.makeText(getActivity(),"t: "+t.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else {
+            Toast.makeText(getActivity(),"No Internet",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public int loadUserId() {
+        int id =0;
+        FileInputStream fis = null;
+        try {
+            fis = getActivity().openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String text;
+            int i = 0;
+            while ((text = br.readLine()) != null) {
+                if(i==1){
+                    id =  Integer.parseInt(text);
+                    break;
+                }
+                i++;
+            }
+            return id;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return id;
+    }
+
+
+    public boolean CheckInternetConnection(){
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
 }

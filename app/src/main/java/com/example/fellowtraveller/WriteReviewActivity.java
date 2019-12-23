27230ad252a7633b94af0575dbc237e86cf01664
SpurosRantoglou <38006349+SpurosRantoglou@users.noreply.github.com>
2019-12-24 01:08:38 +0200

    Layout SmileyRatingBar

--- app/src/main/java/com/example/fellowtraveller/WriteReviewActivity.java
+++ app/src/main/java/com/example/fellowtraveller/WriteReviewActivity.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.hsalf.smilerating.BaseRating;
 import com.hsalf.smilerating.SmileRating;
 
 public class WriteReviewActivity extends AppCompatActivity {
     private ImageButton reliableHappy,reliableNeutral,reliableSad;
-    private SmileRating smileRatingCarefull;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,22 +19,45 @@ public class WriteReviewActivity extends AppCompatActivity {
 
 
 
-        smileRatingCarefull = (SmileRating)findViewById(R.id.smileRatingCarefull);
+        SmileRating smileRatingFriendly = (SmileRating)findViewById(R.id.smileRatingFriendly);
+        SmileRating smileRatingReliable = (SmileRating)findViewById(R.id.smileRatingReliable);
+        SmileRating smileRatingCarefull = (SmileRating)findViewById(R.id.smileRatingCarefull);
+
+        setEmojiNames(smileRatingFriendly);
+        setEmojiNames(smileRatingReliable);
+        setEmojiNames(smileRatingCarefull);
+
+        smileRatingFriendly.setShowLine(false);
+        smileRatingReliable.setShowLine(false);
+        smileRatingCarefull.setShowLine(false);
+
         smileRatingCarefull.setOnSmileySelectionListener(new SmileRating.OnSmileySelectionListener() {
             @Override
-            public void onSmileySelected(int smiley) {
-
+            public void onSmileySelected(int smiley, boolean reselected) {
+                switch (smiley) {
+                    case SmileRating.TERRIBLE:
+                        break;
+                    case SmileRating.BAD:
+                        break;
+                    case SmileRating.OKAY:
+                        break;
+                    case SmileRating.GOOD:
+                        break;
+                    case SmileRating.GREAT:
+                        break;
+                }
             }
+
         });
 
-        reliableHappy = findViewById(R.id.reliable_happy_btn);
-        reliableNeutral = findViewById(R.id.reliable_neutral_btn);
-        reliableHappy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
 
+    }
+    public void setEmojiNames(SmileRating item){
+        item.setNameForSmile(BaseRating.TERRIBLE,"ΠΟΛΥ ΚΑΚΟΣ");
+        item.setNameForSmile(BaseRating.BAD,"ΚΑΚΟΣ");
+        item.setNameForSmile(BaseRating.OKAY,"ΜΕΤΡΙΟΣ");
+        item.setNameForSmile(BaseRating.GOOD,"ΚΑΛΟΣ");
+        item.setNameForSmile(BaseRating.GREAT,"ΠΟΛΥ ΚΑΛΟΣ");
     }
 }

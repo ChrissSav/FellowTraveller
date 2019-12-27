0575d13d64fdf142ed0409b345617c386957b01e
SpurosRantoglou <38006349+SpurosRantoglou@users.noreply.github.com>
2019-12-27 03:57:46 +0200

    ChatConversation Layout

--- app/src/main/java/com/example/fellowtraveller/ChatConversation.java
+++ app/src/main/java/com/example/fellowtraveller/ChatConversation.java
@@ -1,14 +1,85 @@
 package com.example.fellowtraveller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ChatConversation extends AppCompatActivity {
+    private static final String FILE_NAME = "fellow_login_state.txt";
+    private Toolbar convToolabar;
+    private DatabaseReference chatDatabase;
+    private String id, userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_conversation);
+
+        chatDatabase = FirebaseDatabase.getInstance().getReference();
+        userId = getId();
+
+        chatDatabase.child("Chat").child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
+    public String getId(){
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String text;
+            String id ="-1";
+
+            int i = 0;
+            while ((text = br.readLine()) != null) {
+                if (i==1){
+                    id = text;
+                    return id;
+
+                }
+                i++;
+            }
+            //String t = "name : "+name.getText()+"\n"+"email: "+email.getText()+"\n"+"id : "+id;
+            //Toast.makeText(MainHomeActivity.this,t,Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return id;
+    }
+
 }

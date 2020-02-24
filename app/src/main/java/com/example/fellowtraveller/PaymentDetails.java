578e6b68739d7f4d6f649772cd3e9685612e2e86
Spuros <spurakos98@gmail.com>
2020-02-25 00:04:57 +0200

    Paypal payment details #2

--- app/src/main/java/com/example/fellowtraveller/PaymentDetails.java
+++ app/src/main/java/com/example/fellowtraveller/PaymentDetails.java
@@ -2,13 +2,44 @@ package com.example.fellowtraveller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class PaymentDetails extends AppCompatActivity {
+    TextView txtId, txtAmount, txtStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
+
+        txtId = (TextView) findViewById(R.id.txtId);
+        txtAmount = (TextView) findViewById(R.id.txtAmount);
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+
+        //Get Intent
+        Intent intent = getIntent();
+
+        try {
+            JSONObject jsonObject = new JSONObject(intent.getStringExtra("PaymentDetails"));
+            showDetails(jsonObject.getJSONObject("response"), intent.getStringExtra("PaymentAmount"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showDetails(JSONObject response, String paymentAmount) {
+        try {
+            txtId.setText(response.getString("id"));
+            txtAmount.setText("€" + paymentAmount);
+            txtStatus.setText(response.getString("state"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
